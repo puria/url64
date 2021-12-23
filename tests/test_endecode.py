3dcce849e50d2a8b5fe3162337f3621615f8a778
Puria Nafisi Azizi <puria@dyne.org>
2021-12-23 20:08:42 +0100

    feat: ✨ Supporting dicts at encoding time (#1)
    * Enhancing DX
    * Add tests

--- tests/test_endecode.py
+++ tests/test_endecode.py
@@ -6,7 +6,8 @@ import url64
 @pytest.mark.parametrize('value, expected', [
     ("cG9sbA", "poll"),
     ("TUE", "MA"),
-    ("MA", "0")
+    ("MA", "0"),
+    ("eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9", '{"alg": "HS256", "typ": "JWT"}')
 ])
 def test_decode(value, expected):
     assert url64.decode(value) == expected
@@ -17,7 +18,9 @@ def test_decode(value, expected):
     ("poll", "cG9sbA"),
     ("Hello world!", "SGVsbG8gd29ybGQh"),
     ("MA", "TUE"),
-    ("0", "MA")
+    ("0", "MA"),
+    (dict(a=1, b="a", c=[1,"a"]), "eyJhIjogMSwgImIiOiAiYSIsICJjIjogWzEsICJhIl19"),
+    (dict(alg="HS256", typ="JWT"), "eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9")
 ])
 def test_encode(value, expected):
     assert url64.encode(value) == expected
